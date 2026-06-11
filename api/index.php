a9c12bacdcdb5eb9827fa8d1af4799ca06fec85c
ItsMeDiana01 <putrayoga469@gmail.com>
2026-06-11 11:16:33 +0800

    fix: penyesuaian cache storage untuk vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
-// Mengarahkan Vercel untuk membaca sistem utama Laravel yang ada di folder public
+
+// Memberitahu Laravel untuk menggunakan folder sementara (/tmp) karena Vercel bersifat read-only
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+
 require __DIR__ . '/../public/index.php';
